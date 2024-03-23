8b19f602d8ba09faaf8900a9d8097c61a529e71b
SergeEvdokimov <SergeyEwdokimow@ya.ru>
2024-03-23 21:06:30 +0300

    logging_in form released

--- tasks/log_WEB.py
+++ tasks/log_WEB.py
@@ -1,7 +1,8 @@
 from data.jobs import Jobs
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from data import db_session
 from data.users import User
+from forms.user import RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -14,6 +15,30 @@ def index():
     users = session.query(User).all()
     names = {name.id: [name.surname, name.name] for name in users}
     return render_template("index.html", jobs=jobs, names=names)
+
+
+@app.route("/login")
+def logged_in():
+    return render_template('logged_in.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация', form=form, message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация', form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(name=form.name.data, surname=form.surname.data, email=form.email.data, age=form.age.data,
+                    position=form.position.data, speciality=form.speciality.data)
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Register form', form=form)
 
 
 def main():
